eb02ade665e13daa7080ef27428f324380567b26
Ian Buñag <jianbunag@gmail.com>
2026-04-25 19:09:50 -0700

    Implement cache

--- public/index.php
+++ public/index.php
@@ -1,4 +1,9 @@
 <?php require_once __DIR__ . '/../src/index.php'; ?>
+<?php
+    $lastCache = CacheService::memoize(fn() => [time()], ['__METHOD__', 'first argument', 'last argument'], CacheService::FIVE_SECONDS);
+    CacheService::invalidate(['invalidated']);
+    $invalidatedCache = CacheService::memoize(fn() => time(), ['invalidated']);
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,5 +12,9 @@
     </head>
     <body>
         root
+        <br>
+        <?php echo "Last cache: " . $lastCache[0]; ?>
+        <br>
+        <?php echo "Invalidated cache: " . $invalidatedCache; ?>
     </body>
 </html>
