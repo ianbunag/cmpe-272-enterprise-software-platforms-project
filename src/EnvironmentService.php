258f18558d04f35c15acc437a3a77d74d7ca407c
Ian Buñag <jianbunag@gmail.com>
2026-05-03 17:33:49 -0700

    Simplify environment service return type

--- src/EnvironmentService.php
+++ src/EnvironmentService.php
@@ -4,42 +4,42 @@ class EnvironmentService
 {
     private static array $cache = [];
 
-    public static function getDbHost(): array|false|string
+    public static function getDbHost(): false|string
     {
         return self::getCachedEnv('DB_HOST');
     }
 
-    public static function getDbName(): array|false|string
+    public static function getDbName(): false|string
     {
         return self::getCachedEnv('DB_NAME');
     }
 
-    public static function getDbUser(): array|false|string
+    public static function getDbUser(): false|string
     {
         return self::getCachedEnv('DB_USER');
     }
 
-    public static function getDbPassword(): array|false|string
+    public static function getDbPassword(): false|string
     {
         return self::getCachedEnv('DB_PASSWORD');
     }
 
-    public static function getEnableCache(): array|false|string
+    public static function getEnableCache(): false|string
     {
         return self::getCachedEnv('ENABLE_CACHE');
     }
 
-    public static function getVersion(): array|false|string
+    public static function getVersion(): false|string
     {
         return self::getCachedEnv('VERSION');
     }
 
-    public static function getSupportEmail(): array|false|string
+    public static function getSupportEmail(): false|string
     {
         return self::getCachedEnv('SUPPORT_EMAIL');
     }
 
-    private static function getCachedEnv(string $key): array|false|string
+    private static function getCachedEnv(string $key): false|string
     {
         if (!array_key_exists($key, self::$cache)) {
             self::$cache[$key] = getenv($key);
